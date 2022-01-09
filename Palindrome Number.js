d25dfcdcc7e71ed29b8b494db27cfc5703642e5c
dstrickl7 <strickl6@illinois.edu>
2022-01-09 16:32:45 -0600

    added no string solution

--- Palindrome Number.js
+++ Palindrome Number.js
@@ -1,4 +1,3 @@
-/*Palindrome Number*/
 /*
 Given an integer x, return true if x is palindrome integer.
 Constraints: -231 <= x <= 231 - 1
@@ -31,3 +30,49 @@ function isPalindromeNum(integer) {
   }
 }
 /**************************************************************/
+/*No strings version */
+//Uses slightly less memory, but considerably slower
+/*****Thought process for solving*****/
+/*
+  1)Divide integer to get individual numbers
+  2)create a copy of array
+  3)reverse copied array
+  4)compare array and reversed array
+   4.1)Loop through both arrays
+  */
+
+function splitNum(number) {
+  /*-loop through number
+     -take the remainder of integer divided by a number (should give either first or last digit) and put it into an array
+     -when modulo= 0, end loop */
+  let numArr = []; //initialize an empty array
+  let numberCopy = number; //make a copy of original input
+  while (numberCopy / 10 !== 0) {
+    //Check that number is not 0
+    numArr.push(numberCopy % 10); //Take remainder of number divided by 10 and put into array
+    numberCopy = Math.floor(numberCopy / 10); //divide number by 10 and set it equal to the integer rounded down
+  }
+  /*array returned from loop is in reverse order */
+  return numArr.reverse(); //return array in correct order
+}
+
+function isPalindromeNumNoString(integer) {
+  if (integer < 0) {
+    return false;
+  } else {
+    let integerArr = splitNum(integer);
+    let reversedArr = integerArr.slice().reverse();
+    let falseFound = 0;
+    integerArr.forEach((value, a) => {
+      if (value !== reversedArr[a]) {
+        falseFound++;
+        return;
+      }
+    });
+    if (falseFound) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
